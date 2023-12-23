038399d76dcbf234e4a74ee9663d6ff5aef96937
Yuki Watanabe <ukwhatn@gmail.com>
2023-12-23 22:32:12 +0900

    init

---
+++ main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var href = window.location.href;
+    var hrefRegExp = /\?_?[A-Za-z0-9\-=]+/g;
+    var link = "http://scp-jp.wikidot.com/";
+
+    if (hrefRegExp.test(href)) {
+        var arguments = href.slice(href.search(hrefRegExp) + 1);
+
+        link = replace(arguments)
+
+        window.location.replace(link);
+    } else {
+        alert("SCP-JPにリダイレクトします");
+        window.location.replace('http://scp-jp.wikidot.com/');
+    }
+
+    function replace(arguments) {
+        if (/=/g.test(arguments)) {
+            var args = arguments.split("&");
+            var name, site, scheme;
+            for (i = 0; i < args.length; i++) {
+                if (/name=[A-Za-z0-9\-]+/g.test(args[i])) {
+                    name = args[i].slice(args[i].search(/=[A-Za-z0-9\-]+/g) + 1);
+                }
+                if (/site=[A-Za-z0-9\-]+/g.test(args[i])) {
+                    site = args[i].slice(args[i].search(/=[A-Za-z0-9\-]+/g) + 1);
+                }
+                if (/scheme=[A-Za-z0-9\-]+/g.test(args[i])) {
+                    scheme = args[i].slice(args[i].search(/=[A-Za-z0-9\-]+/g) + 1);
+                }
+            }
+            console.log("name = "+ name);
+            console.log("site = "+ site);
+            console.log("scheme = "+ scheme);
+            if (site == undefined) {
+                site = "scp-jp";
+            }
+            if (scheme == undefined) {
+                scheme = "http";
+            }
+            link = scheme + "://" + site + ".wikidot.com/" + name;
+        } else {
+            link = "https://scp-jp.wikidot.com/" + arguments;
+        }
+        return link;
+    }
+})
